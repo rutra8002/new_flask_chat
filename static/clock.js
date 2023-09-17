643d2239d57ec9cc390a03b927c67a64af500ef7
admin <arturkummer08@gmail.com>
2023-09-17 09:58:12 +0200

    MOVING CLOCK

--- static/clock.js
+++ static/clock.js
@@ -16,3 +16,33 @@ function updateClock() {
 
 // Update the clock every second
 setInterval(updateClock, 1000);
+
+var clock = document.getElementById("clock");
+var isDragging = false;
+var offset = { x: 0, y: 0 };
+
+clock.addEventListener("mousedown", startDrag);
+clock.addEventListener("mousemove", drag);
+clock.addEventListener("mouseup", stopDrag);
+clock.addEventListener("mouseleave", stopDrag);
+
+function startDrag(e) {
+  isDragging = true;
+  offset.x = e.clientX - clock.offsetLeft;
+  offset.y = e.clientY - clock.offsetTop;
+}
+
+function drag(e) {
+  if (isDragging) {
+    var left = e.clientX - offset.x;
+    var top = e.clientY - offset.y;
+    var right = left + clock.offsetWidth;
+
+    clock.style.left = left + "px";
+    clock.style.top = top + "px";
+    clock.style.right = "auto";
+  }
+}
+function stopDrag() {
+  isDragging = false;
+}
